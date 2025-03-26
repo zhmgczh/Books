7817a1a3d75ecc62db10da774f08d474c1ce6cd3
蔡于飛 <54766809+zhmgczh@users.noreply.github.com>
2025-03-25 20:42:04 -0400

    Update.

--- 歷史小說/中國歷代通俗演義（校對）/convert_to_HTML.py
+++ 歷史小說/中國歷代通俗演義（校對）/convert_to_HTML.py
@@ -1,11 +1,42 @@
-def main(file_name: str = "東周列國志演義（余邵魚）.txt"):
-    with open(file_name, mode="r", encoding="utf-8") as input:
+def main(
+    file_name: str = "東周列國志演義（余邵魚）", catalog_tag: str = "目錄"
+) -> None:
+    with open(file_name + ".txt", mode="r", encoding="utf-8") as input:
         lines = input.readlines()
-        for line in lines:
-            if "\n" == line[-1]:
-                line = line[:-1]
-            if line != "":
-                print("<p>" + line + "</p>")
+        block_count = 0
+        block_first = True
+        catalog = False
+        catalog_count = 0
+        with open(file_name + ".html", mode="w", encoding="utf-8") as output:
+            output.write(
+                '<!DOCTYPE html>\n<html>\n<head>\n<meta charset="utf-8">\n<title>'
+                + file_name
+                + "</title>\n</head>\n<body>\n<h1>"
+                + file_name
+                + "</h1>\n"
+            )
+            for line in lines:
+                if "\n" == line[-1]:
+                    line = line[:-1]
+                if catalog and line not in ("", "　　※※※"):
+                    output.write(
+                        f'<p><a href="#section_{catalog_count+1}">'
+                        + line
+                        + "</a></p>\n"
+                    )
+                    catalog_count += 1
+                elif block_first and "" != line:
+                    output.write(f'<h2 id="section_{block_count}">' + line + "</h2>\n")
+                    block_first = False
+                elif "" != line:
+                    output.write("<p>" + line + "</p>\n")
+                if catalog_tag == line:
+                    catalog = True
+                elif "　　※※※" == line:
+                    catalog = False
+                    block_count += 1
+                    block_first = True
+            output.write("</body>\n</html>")
 
 
 if "__main__" == __name__:
